ea1a88e802a081d9b882b9eb108137e19612684b
Jonathan S <jojoschi98@gmail.com>
2023-09-30 14:47:35 +0200

    Revert split to 80/10/10.

--- src/data/build_yolo_dataset.py
+++ src/data/build_yolo_dataset.py
@@ -56,7 +56,7 @@ for img_lbl in parsed_json:
 
 
 # Create train/validation/test split
-ds_tools.create_split(label_file, SEED, train_p=0.8, val_p=0.15, test_p=0.5)
+ds_tools.create_split(label_file, SEED)
 
 # Create dataset description
 ds_tools.create_ds_yaml(label_file)
